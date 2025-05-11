54e6809362b978de2b97ed770c38e723722c945b
Alvaronaaval <alvaronavalrodriguez@gmail.com>
2025-05-11 15:48:28 +0200

    added quickstab attack

--- com/combatgame/models/objects/attackdecorators/dagger/QuickStab.java
+++ com/combatgame/models/objects/attackdecorators/dagger/QuickStab.java
@@ -2,20 +2,21 @@ package com.combatgame.models.objects.attackdecorators.dagger;
 import com.combatgame.models.objects.Weapon;
 import com.combatgame.models.objects.Attack;
 
-public class  Slash implements Attack {
+public class QuickStab implements Attack {
     private Weapon weapon;
 
-    public Slash(Weapon weapon) {
+    public QuickStab(Weapon weapon) {
         this.weapon = weapon;
     }
 
     @Override
     public void executeAttack() {
-        System.out.println("Executing Slash!");
+        System.out.println("Executing Quick Stab!");
     }
 
     @Override
     public int getDamage() {
-        return weapon.getDamage() + 2; // Slash adds 2 damage
+        return weapon.getDamage(); // Quick Stab adds 3 damage
+        // function to apply wounded status effect
     }
 }
