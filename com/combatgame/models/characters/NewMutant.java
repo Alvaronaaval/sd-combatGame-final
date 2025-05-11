81258a18e55370a84547597ca4bbd91c3af894b9
Pingu000 <tortualex10@gmail.com>
2025-05-11 19:16:18 +0200

    fixed class

--- com/combatgame/models/characters/NewMutant.java
+++ com/combatgame/models/characters/NewMutant.java
@@ -1,17 +1,17 @@
 package com.combatgame.models.characters;
-import com.combatgame.models.Attributes;
+import com.combatgame.models.characters.Attributes;
 import com.combatgame.models.WorldEffect;
 
 public class NewMutant extends AbstractEnemy {
     public NewMutant(WorldEffect worldEffect) {
-        super(worldEffect, attributes.randomAttributes()); // crea atributos aleatorios
+        super(worldEffect, Attributes.randomAttributes());// crea atributos aleatorios
     }
 
     @Override
     public void takeTurn() {
         System.out.println("Mutant performs an unpredictable action!");
         if (worldEffect == WorldEffect.VOLCANO) {
-            attributes.getHealth() += 5;
+            attributes.setHealth(attributes.getHealth() + 5);
             System.out.println("Mutant regenerates health!");
         }
     }
